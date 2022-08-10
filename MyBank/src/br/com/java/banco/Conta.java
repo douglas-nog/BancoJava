873708feba61a128c6b0f2a5498af629b6a5a605
Douglas <douglaswnogueira@hotmai.com>
2022-08-10 12:49:46 -0300

    Testes realizados

--- MyBank/src/br/com/java/banco/Conta.java
+++ MyBank/src/br/com/java/banco/Conta.java
@@ -7,4 +7,28 @@ public class Conta {
 	double saldo;
 	
 	
+	public Conta() {
+		
+	}
+		
+	public Conta(int agencia, int numero, double saldo) {
+		this.agencia = agencia;
+		this.numero = numero;
+		this.saldo = saldo;
+	}
+
+
+
+	public void depositar(double valor) {
+		saldo += valor;
+	}
+	
+	public void retirar(double valor) {
+		saldo -= valor; 
+	}
+	
+	public double verificarSaldo() {
+		return saldo;
+	}
+	
 }
--- MyBank/src/br/com/java/banco/Teste.java
+++ MyBank/src/br/com/java/banco/Teste.java
@@ -1,5 +1,22 @@
 package br.com.java.banco;
 
 public class Teste {
-
+	
+	public static void main(String[] args) {
+	
+		Conta cc = new Conta();
+		cc.agencia = 1234;
+		cc.numero = 147258;
+		cc.saldo = 1000.0; 
+		
+		cc.depositar(100);
+		cc.retirar(50);
+		System.out.println(cc.verificarSaldo());
+		
+		Conta poupanca = new Conta(4567, 369258, 500);
+		
+		poupanca.depositar(5000);
+		poupanca.retirar(500);
+		System.out.println(poupanca.verificarSaldo());
+	}
 }
